330cdb51f0fab20665a0eef8e862a77b80812af8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-13 05:50:59 -0300

    chore: update storybook to 6.0.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/preset-typescript', 
-    '@storybook/addon-actions', 
+    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
   ],
